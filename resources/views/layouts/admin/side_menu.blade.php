b7f16a3fe82160f5aba05219a8af56ef830f2525
trungcheng <trungs1bmt@gmail.com>
2017-11-17 17:20:13 +0700

    permission and role

--- resources/views/layouts/admin/side_menu.blade.php
+++ resources/views/layouts/admin/side_menu.blade.php
@@ -45,35 +45,23 @@
             </li>
             
             <li @if( $menu=='site_configurations') class="active" @endif >
+                <a href="{!! URL::action('Admin\RolePermissionController@index') !!}">
+                    <i class="fa fa-cogs"></i> <span>Quản lý quyền hạn</span>
+                </a>
+            </li>
+
+            <li @if( $menu=='site_configurations') class="active" @endif >
+                <a href="{!! URL::action('Admin\ChannelController@index') !!}">
+                    <i class="fa fa-cogs"></i> <span>Quản lý channels</span>
+                </a>
+            </li>
+
+            <li @if( $menu=='site_configurations') class="active" @endif >
                 <a href="{!! URL::action('Admin\SiteConfigurationController@index') !!}">
                     <i class="fa fa-cogs"></i> <span>Cấu hình site</span>
                 </a>
             </li>
             @endrole
-            
-            <li @if( $menu=='article') class="active" @endif >
-                <a href="{!! URL::action('Admin\ArticleController@index') !!}">
-                    <i class="fa fa-file-word-o"></i> <span>Quản lý bài viết</span>
-                </a>
-            </li>
-            
-            <li @if( $menu=='user_notification') class="active" @endif >
-                <a href="{!! URL::action('Admin\UserNotificationController@index') !!}">
-                    <i class="fa fa-bell"></i> <span>Quản lý thông báo người dùng</span>
-                </a>
-            </li>
-            
-            <li @if( $menu=='admin_user_notification') class="active" @endif >
-                <a href="{!! URL::action('Admin\AdminUserNotificationController@index') !!}">
-                    <i class="fa fa-bell-o"></i> <span>Quản lý thông báo user admin</span>
-                </a>
-            </li>
-            
-            <li @if( $menu=='image') class="active" @endif >
-                <a href="{!! URL::action('Admin\ImageController@index') !!}">
-                    <i class="fa fa-file-image-o"></i> <span>Quản lý ảnh</span>
-                </a>
-            </li>
             <!-- %%SIDEMENU%% -->
         </ul>
     </section>
